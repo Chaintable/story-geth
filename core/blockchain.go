84a71ef55460f8c52dfaa4fe4059e2cea100ef34
0byteme <819643718@qq.com>
2026-04-14 15:37:40 +0800

    fix: is backup panic

--- core/blockchain.go
+++ core/blockchain.go
@@ -30,6 +30,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/Chaintable/pipeline/leader"
 	"github.com/Chaintable/pipeline/tracer"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/lru"
@@ -1779,8 +1780,14 @@ func (bc *BlockChain) pushBlockChange(block *types.Block) {
 	// 先确保 pipeline tracer 不为空，然后再判断是否需要push kafka
 	// 上一个push kafka的block, 必然存在(至少有genesis block)
 	// 上一个push kafka的block比当前的head block还要新，说明有unwind回退，不需要处理, 即使是fork，等有更新的block的时候再一起push
-	if tracer.NodeXPusher != nil && tracer.NodeXPusher.LastPushedBlock().BlockNumber <= block.NumberU64() {
-		lastPushBlock := tracer.NodeXPusher.LastPushedBlock()
+	if tracer.NodeXPusher == nil {
+		return
+	}
+	isLeader := leader.GlobalManager.IsLeader()
+	leader.GlobalManager.RLock()
+	lastPushBlock := tracer.NodeXPusher.LastPushedBlock()
+	leader.GlobalManager.RUnlock()
+	if isLeader && lastPushBlock != nil && lastPushBlock.BlockNumber <= block.NumberU64() {
 		_, dropBlocks, newBlocks := bc.getCommonAncestor(*lastPushBlock, ptypes.BlockContext{
 			BlockNumber: block.NumberU64(),
 			Hash:        block.Hash(),
